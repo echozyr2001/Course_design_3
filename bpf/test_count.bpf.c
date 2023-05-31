7f9862a336267eed13b948a2788818babeff9018
echozyr2001 <echo.zyr.2001@gmail.com>
2023-06-01 03:35:23 +0800

    update count_test

--- bpf/test_count.bpf.c
+++ bpf/test_count.bpf.c
@@ -21,7 +21,7 @@ struct {
 } values SEC(".maps");
 
 SEC("tracepoint/syscalls/sys_enter_open")
-int count_open(struct syscall *args)
+int count_open(void *args)
 {
   u32 key = 0;
   u64 *value = bpf_map_lookup_elem(&values, &key);
